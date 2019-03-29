3b3563f19c687fd9ca24beeac5d5263474073e27
RaulSul <raul.sulaimanov@gmail.com>
2019-03-29 11:46:52 +0100

    added project 7

---
+++ Project-7-Whitehouse-Petitions/Project-7-Whitehouse-Petitions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project-7-Whitehouse-Petitions
+//
+//  Created by Raul Sulaimanov on 29.03.19.
+//  Copyright © 2019 Raul Sulaimanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
