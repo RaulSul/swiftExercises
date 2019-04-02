f6cf98dad6829633c729415f2f2a04c401f9bdb3
RaulSul <raul.sulaimanov@gmail.com>
2019-04-02 11:59:09 +0200

    added project 10

---
+++ Project-10-Names-to-Faces/Project-10-Names-to-Faces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project-10-Names-to-Faces
+//
+//  Created by Raul Sulaimanov on 02.04.19.
+//  Copyright © 2019 Raul Sulaimanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
