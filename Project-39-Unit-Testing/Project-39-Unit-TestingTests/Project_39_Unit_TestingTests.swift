f7fb34958f5201a19006f89876c0a6f6adc321b4
RaulSul <raul.sulaimanov@gmail.com>
2019-04-04 13:39:38 +0200

    added Project 39

---
+++ Project-39-Unit-Testing/Project-39-Unit-TestingTests/Project_39_Unit_TestingTests.swift
@@ -0,0 +1,34 @@
+//
+//  Project_39_Unit_TestingTests.swift
+//  Project-39-Unit-TestingTests
+//
+//  Created by Raul Sulaimanov on 04.04.19.
+//  Copyright © 2019 Raul Sulaimanov. All rights reserved.
+//
+
+import XCTest
+@testable import Project_39_Unit_Testing
+
+class Project_39_Unit_TestingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
