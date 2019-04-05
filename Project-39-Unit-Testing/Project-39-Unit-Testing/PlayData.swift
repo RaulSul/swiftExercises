6639c983f4c373162054259f7e4081aed64b2312
RaulSul <raul.sulaimanov@gmail.com>
2019-04-05 14:58:43 +0200

    refactored the code, added sorting function, added new test

--- Project-39-Unit-Testing/Project-39-Unit-Testing/PlayData.swift
+++ Project-39-Unit-Testing/Project-39-Unit-Testing/PlayData.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class PlayData {
     var allWords = [String]()
-    var wordCounts = [String: Int]()
+    var wordCounts: NSCountedSet!
     
     init() {
         if let path = Bundle.main.path(forResource: "plays", ofType: "txt") {
@@ -20,15 +20,9 @@ class PlayData {
             }
         }
         
-        for word in allWords {
-            if wordCounts[word] == nil {
-                wordCounts[word] = 1
-            } else {
-                wordCounts[word]! += 1
-            }
-        }
-        
-        allWords = Array(wordCounts.keys)
+        wordCounts = NSCountedSet(array: allWords)
+        let sorted = wordCounts.allObjects.sorted { wordCounts.count(for: $0) > wordCounts.count(for: $1) }
+        allWords = sorted as! [String]
     }
     
     
